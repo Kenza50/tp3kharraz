9f8c47e8c20cac57899aa220f123b15695a751be
Kenza Kharraz <Kenza.kharraz5@gmail.com>
2025-11-04 15:00:43 +0100

    Initial commit

---
+++ src/main/java/ma/emsi/kharraz/tp3kharraz/HelloApplication.java
@@ -0,0 +1,9 @@
+package ma.emsi.kharraz.tp3kharraz;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
