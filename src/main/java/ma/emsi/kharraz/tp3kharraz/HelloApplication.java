869909b87702c952a8b2cd60b01dbac936d1e804
Kenza Kharraz <Kenza.kharraz5@gmail.com>
2025-11-07 23:37:10 +0100

    ajout de guide touristique

--- src/main/java/ma/emsi/kharraz/tp3kharraz/HelloApplication.java
+++
@@ -1,9 +0,0 @@
-package ma.emsi.kharraz.tp3kharraz;
-
-import jakarta.ws.rs.ApplicationPath;
-import jakarta.ws.rs.core.Application;
-
-@ApplicationPath("/api")
-public class HelloApplication extends Application {
-
-}
